695f2f4eb540c234be64433d2deeeff35f2b64d4
lvguocai <guocai_lv@qingsongchou.com>
2021-05-23 15:16:54 +0800

    六车三位 acquire release

--- src/main/java/com/zhouyang/countDownLatch/SemaphoreDemo.java
+++ src/main/java/com/zhouyang/countDownLatch/SemaphoreDemo.java
@@ -4,21 +4,16 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @program: learnning
- * @description: 抢车位
- * @author: Lv
- * @create: 2020-12-09 09:53
+ *  * 六车三位
+ * 初始化一个信号量为3，默认是false 非公平锁， 模拟3个停车位
+ * semaphore.acquire release
+ * 即，多人抢多个资源问题；
  **/
 public class SemaphoreDemo {
 
     public static void main(String[] args) {
 
-        /**
-         * 初始化一个信号量为3，默认是false 非公平锁， 模拟3个停车位
-         * 即，多人抢多个资源问题；
-         */
         Semaphore semaphore = new Semaphore(3, false);
-
         // 模拟6部车
         for (int i = 0; i < 6; i++) {
             new Thread(() -> {
@@ -27,13 +22,10 @@ public class SemaphoreDemo {
                     semaphore.acquire(); // 抢占
 
                     System.out.println(Thread.currentThread().getName() + "\t 抢到车位");
-
                     // 每个车停3秒
                     try {
                         TimeUnit.SECONDS.sleep(3);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    } catch (InterruptedException e) { e.printStackTrace(); }
 
                     System.out.println(Thread.currentThread().getName() + "\t 离开车位");
 
